20a07f44c7092dd32fabeef4cf55e73fcc5dfaec
Jakub Baron <jakubaron@gmail.com>
2018-10-28 14:55:54 +0000

    Modified hello world button :)

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 export class HelloWorld extends Component {
     render() {
         return (
-          <Button variant="contained" color="primary">
-            Hello World
-          </Button>
+          <Grid container
+            direction="row"
+            justify="center"
+            alignItems="center"
+            spacing = {12}
+          >
+            <Button variant="contained" color="primary">
+              Hello World
+            </Button>
+          </Grid>
         );
     }
 }
